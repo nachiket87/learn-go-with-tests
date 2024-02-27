afd71ade133df2d1497a9569912815bccb17511b
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-02-27 11:20:59 -0500

    add spanish

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,19 +4,27 @@ import "testing"
 
 func TestHello(t *testing.T) {
   t.Run("saying hello to people", func(t *testing.T) {
-    got := Hello("Nachiket")
+    got := Hello("Nachiket", "")
     want := "Hello, Nachiket"
+    assetCorrectMessage(t, got, want)
 
-    if got != want {
-      t.Errorf("got %q want %q", got, want)
-    }
   })
   t.Run("say 'Hello, World' when empty string is supplied", func(t *testing.T) {
-    got := Hello("")
+    got := Hello("", "")
     want := "Hello, World"
-
-    if got != want {
-      t.Errorf("got %q want %q", got, want)
-    }
+    assetCorrectMessage(t, got, want)
   })
+
+  t.Run("say 'Hello' in Spanish", func(t *testing.T) {
+    got := Hello("Elodie", "Spanish")
+    want := "Hola, Elodie"
+    assetCorrectMessage(t, got, want)
+  })
+}
+
+func assetCorrectMessage(t testing.TB, got, want string) {
+  t.Helper()
+  if got != want {
+    t.Errorf("got %q want %q", got, want)
+  }
 }
