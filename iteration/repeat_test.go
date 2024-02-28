0817e41028937831a6d304220efc97dc6bb89539
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-02-28 12:23:14 -0500

    format lint

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -6,22 +6,22 @@ import (
 )
 
 func TestRepeat(t *testing.T) {
-  repeated := Repeat("a", 6)
-  expected := "aaaaaa"
-  if repeated != expected {
-    t.Errorf("expected '%s' but got '%s'", expected, repeated)
-  }
+	repeated := Repeat("a", 6)
+	expected := "aaaaaa"
+	if repeated != expected {
+		t.Errorf("expected '%s' but got '%s'", expected, repeated)
+	}
 }
 
 func BenchmarkRepeat(b *testing.B) {
-  for i := 0; i < b.N; i++ {
-    Repeat("a", 5)
-  }
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
 }
 
 func ExampleRepeat() {
-  repeated := Repeat("a", 7)
-  fmt.Println(repeated)
-  // Output: aaaaaaa
-  
+	repeated := Repeat("a", 7)
+	fmt.Println(repeated)
+	// Output: aaaaaaa
+
 }
