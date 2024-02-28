e9a3e25e18df0dfadb2d42036ce195ba49f3fbd2
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-02-28 12:20:08 -0500

    add dynamic count of iterations

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,8 +3,8 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-  repeated := Repeat("a")
-  expected := "aaaaa"
+  repeated := Repeat("a", 6)
+  expected := "aaaaaa"
   if repeated != expected {
     t.Errorf("expected '%s' but got '%s'", expected, repeated)
   }
@@ -12,6 +12,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
   for i := 0; i < b.N; i++ {
-    Repeat("a")
+    Repeat("a", 5)
   }
 }
