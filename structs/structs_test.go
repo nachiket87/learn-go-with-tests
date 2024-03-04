a22cce216e4c3c61149e5dfeeabc08b1949f5f90
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-03-04 11:14:27 -0500

    add shape interface

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -3,8 +3,9 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
+  rectangle := Rectangle{10.0, 10.0}
   want := 40.0
-  got := Perimeter(10.0, 10.0)
+  got := Perimeter(rectangle)
 
   if got != want {
     t.Errorf("got %.2f want %.2f", got, want)
@@ -13,11 +14,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-  want := 100.0
-  got := Area(10.0, 10.0)
 
-  if got != want {
-    t.Errorf("got %.2f want %.2f", got, want)
-  }
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
 
 }
