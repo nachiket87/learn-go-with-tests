b5432b6a447e36ed3d8302015e59b393054ce733
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-03-04 13:20:54 -0500

    add better tests

--- structs/structs.go
+++ structs/structs.go
@@ -11,6 +11,11 @@ type Circle struct {
   Radius float64
 }
 
+type Triangle struct {
+  Height float64
+  Base float64
+}
+
 type Shape interface {
   Area() float64
 }
@@ -21,6 +26,10 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
   return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+  return 0.5 * t.Base * t.Height
 }
 
 func Perimeter(rectangle Rectangle) float64 {
