131649f004342356919a41b1ff221eeb7c11d7ab
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-03-11 11:31:59 -0400

    v3 refactor

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,17 +3,32 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-  dictionary := Dictionary{ "test": "this is just a test" }
+	dictionary := Dictionary{"test": "this is just a test"}
 
-  got := dictionary.Search("test")
-  want := "this is just a test"
-  assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
+	})
+
 }
-
 func assertStrings(t testing.TB, got, want string) {
-  t.Helper()
-  if got != want {
-    t.Errorf("got %q want %q given %q", got, want, "test")
-  }
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q given %q", got, want, "test")
+	}
 
+}
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
 }
