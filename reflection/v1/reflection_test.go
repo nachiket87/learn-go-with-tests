f49df21452c624cb95d5fe34e85ad8cc02f4c523
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-03-18 12:04:08 -0400

    update reflection test

--- reflection/v1/reflection_test.go
+++ reflection/v1/reflection_test.go
@@ -1,20 +1,42 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	expected := "Nachiket"
-	var got []string
-
-	x := struct {
-		Name string
-	}{expected}
-
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-	if got[0] != expected {
-		t.Errorf("got %q, want %q", got[0], expected)
+  cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+    {
+      "struct with two string fields",
+      struct {
+        Name string
+        City string
+      } {"Chris", "London"},
+      []string{"Chris", "London"},
+    },
 	}
 
+  for _, test := range cases {
+    t.Run(test.Name, func(t *testing.T) {
+      var got []string
+      walk(test.Input, func(input string) {
+        got = append(got, input)
+      })
+      if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+    })
+  }
 }
