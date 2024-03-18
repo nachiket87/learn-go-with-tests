61f9ac6a836a6f93399cb2414345aca4e2b4da72
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-03-18 09:20:34 -0400

    create helper for measuring time

--- select/racer.go
+++ select/racer.go
@@ -6,17 +6,23 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-  startA := time.Now()
-  http.Get(a)
-  aDuration := time.Since(startA)
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
 
-  startB := time.Now()
-  http.Get(b)
-  bDuration := time.Since(startB)
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
 
-  if aDuration < bDuration {
-    return a
-  }
+	if aDuration < bDuration {
+		return a
+	}
 
-  return b
+	return b
+}
+
+func MeasureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }
