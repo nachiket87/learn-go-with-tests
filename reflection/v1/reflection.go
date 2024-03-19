aaed75efffd9eb9502f1b57ec6e2d58476b30d35
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-03-19 10:40:48 -0400

    only check string values

--- reflection/v1/reflection.go
+++ reflection/v1/reflection.go
@@ -1,9 +1,15 @@
 package main
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func walk(x interface{}, fn func(input string)) {
-  val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+	val := reflect.ValueOf(x)
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+	}
 }
