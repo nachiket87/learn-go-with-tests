960d0f543d65dbed9c3e23af3bca1bd831f256a3
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-03-19 11:09:23 -0400

    nested structs

--- reflection/v1/reflection.go
+++ reflection/v1/reflection.go
@@ -5,11 +5,24 @@ import (
 )
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
+	val := getValue(x)
+
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 		if field.Kind() == reflect.String {
 			fn(field.String())
 		}
+		if field.Kind() == reflect.Struct {
+			walk(field.Interface(), fn)
+		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	return val
+
 }
--- reflection/v1/reflection_test.go
+++ reflection/v1/reflection_test.go
@@ -5,8 +5,19 @@ import (
 	"testing"
 )
 
+
+type Profile struct {
+  Age int
+  City string
+}
+
+type Person struct {
+  Name string
+  Profile Profile
+}
+
 func TestWalk(t *testing.T) {
-  cases := []struct {
+	cases := []struct {
 		Name          string
 		Input         interface{}
 		ExpectedCalls []string
@@ -18,33 +29,43 @@ func TestWalk(t *testing.T) {
 			}{"Chris"},
 			[]string{"Chris"},
 		},
+		{
+			"struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"struct with non string value",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		{
+			"nested fields",
+      Person{"Chris", Profile{33, "London"}},
+			[]string{"Chris", "London"},
+		},
     {
-      "struct with two string fields",
-      struct {
-        Name string
-        City string
-      } {"Chris", "London"},
+      "pointer to value",
+      &Person{"Chris", Profile{33, "London"}},
       []string{"Chris", "London"},
-    },
-    {
-      "struct with non string value",
-      struct {
-        Name string
-        Age int
-      } {"Chris", 33},
-      []string{"Chris"},
     },
 	}
 
-  for _, test := range cases {
-    t.Run(test.Name, func(t *testing.T) {
-      var got []string
-      walk(test.Input, func(input string) {
-        got = append(got, input)
-      })
-      if !reflect.DeepEqual(got, test.ExpectedCalls) {
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
 				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
 			}
-    })
-  }
+		})
+	}
 }
