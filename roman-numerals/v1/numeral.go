98179380f8223abde0316255a69562734091fda3
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-03-21 12:53:24 -0400

    roman numerals till 39

--- roman-numerals/v1/numeral.go
+++ roman-numerals/v1/numeral.go
@@ -2,23 +2,28 @@ package main
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
-  
-  var result strings.Builder
 
+	var result strings.Builder
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
+		}
+	}
 
-  for arabic > 0 {
-    switch {
-    case arabic > 4:
-      result.WriteString("V")
-      arabic -= 5
-    case arabic > 3:
-      result.WriteString("IV")
-      arabic -= 4
-    default:
-      result.WriteString("I")
-      arabic--
-    }
-  }
-  return result.String()
+	return result.String()
 }
