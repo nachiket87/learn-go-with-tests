d4a02625be689adab1d865842fb22c181fa2ecd6
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-03-22 16:11:45 -0400

    add acceptance test

---
+++ clockface/v3/clockface/clockface.go
@@ -0,0 +1,39 @@
+package clockface
+
+import (
+	"math"
+	"time"
+)
+
+// A Point represents a two dimensional Cartesian coordinate.
+type Point struct {
+	X float64
+	Y float64
+}
+
+// SecondHand is the unit vector of the second hand of an analogue clock at time `t`.
+// represented as a Point.
+func SecondHand(t time.Time) Point {
+	return Point{150, 60}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / (float64(t.Second()))))
+}
+
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
+}
+
+func roughlyEqualFloat64(a, b float64) bool {
+	const equalityThreshold = 1e-7
+	return math.Abs(a-b) < equalityThreshold
+}
+
+func roughlyEqualPoint(a, b Point) bool {
+	return roughlyEqualFloat64(a.X, b.X) && roughlyEqualFloat64(a.Y, b.Y)
+}
