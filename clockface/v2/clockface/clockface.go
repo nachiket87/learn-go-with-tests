a17bca4b221900441fe93254b3d832ab268370ee
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-03-22 15:30:47 -0400

    make tests pass for secondsinRadians

--- clockface/v2/clockface/clockface.go
+++ clockface/v2/clockface/clockface.go
@@ -1,6 +1,9 @@
 package clockface
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 // A Point represents a two dimensional Cartesian coordinate.
 type Point struct {
@@ -10,6 +13,7 @@ type Point struct {
 
 // SecondHand is the unit vector of the second hand of an analogue clock at time `t`.
 // represented as a Point.
-func SecondHand(t time.Time) Point {
-	return Point{150, 60}
+
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi
 }
--- clockface/v2/clockface/clockface_test.go
+++ clockface/v2/clockface/clockface_test.go
@@ -1,28 +1,15 @@
-package clockface_test
+package clockface
 
 import (
-	"clockface"
+	"math"
 	"testing"
 	"time"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
-	}
-}
-
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-	want := clockface.Point{X: 150, Y: 150 + 90}
-
-	got := clockface.SecondHand(tm)
+func TestSecondsInRadian(t *testing.T) {
+	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+	want := math.Pi
+	got := secondsInRadians(thirtySeconds)
 
 	if got != want {
 		t.Errorf("got %v, want %v", got, want)
