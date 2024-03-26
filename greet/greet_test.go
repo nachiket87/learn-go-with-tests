b7636fc49d54d68efbfb41361a30fa7c411b0217
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2024-03-26 19:36:39 -0400

    link hello world

--- greet/greet_test.go
+++ greet/greet_test.go
@@ -6,15 +6,13 @@ import (
 )
 
 func TestGreet(t *testing.T) {
-  buffer := bytes.Buffer{}
-  Greet(&buffer, "Nachi")
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Nachi")
 
-  got := buffer.String()
-  want := "Hello, Nachi"
+	got := buffer.String()
+	want := "Hello, Nachi"
 
-  if got != want {
-    t.Errorf("got %q want %q", got, want)
-  }
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
-
-
